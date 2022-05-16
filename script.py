7409a95e8496ee0f982f38629be60c61d19a8247
Tysudo <91703892+tysudo@users.noreply.github.com>
2022-05-16 16:59:48 -0400

    Update script.py

--- script.py
+++ script.py
@@ -2,27 +2,44 @@ import sys
 import os
 
 
+
+    
+    
 def main():
-    var = (str(input('What are operating system are you on? (Windows, Mac, Linux CaSe SeNsItIvE)')))
-    
-    if var == 'Windows':
-        windows()
-        
-    if var == 'Mac':
-        mac()
-        
-    if var == 'Linux':
-        linux()
-    
-    
-    
-def windows():
     print('(1) Developer Stuff')
     print('(2) Search ')
-    print('(3) ')
+    print('(3) Download Github Repo')
+    
+    var = (int(input('What would you like to do? ')))
+    
+    if var == 1:
+        dev()
+        
+    if var == 2:
+        search()
+    
+    if var == 3:
+        github()
+        
+
+####################################################################################################################################################################################
     
     
+def dev():
+    print('(1) Open Vscode')
+    print('(2) Open Github this repo')    
+    print('(3) Open StackOverflow')
     
+    var2 = (int(input('What would you like to do? ')))
+    
+    if var2 == 1:
+        os.system('code')
+        
+    if var2 == 2:
+        os.system('start chrome github.com/tysudo/Help-Script-3.0.0')
+        
+    if var2 == 3:
+        os.system('start chrome https://stackoverflow.com/')
     
     
     
