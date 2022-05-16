c8e4e633fedb4520573db18969037b7093ba8269
Tysudo <91703892+tysudo@users.noreply.github.com>
2022-05-15 20:37:03 -0400

    Create script.py

---
+++ script.py
@@ -0,0 +1,34 @@
+import sys
+import os
+
+
+def main():
+    var = (str(input('What are operating system are you on? (Windows, Mac, Linux CaSe SeNsItIvE)')))
+    
+    if var == 'Windows':
+        windows()
+        
+    if var == 'Mac':
+        mac()
+        
+    if var == 'Linux':
+        linux()
+    
+    
+    
+def windows():
+    print('(1) Developer Stuff')
+    print('(2) Search ')
+    print('(3) ')
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+main()
